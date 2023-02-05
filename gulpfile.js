36d57b73dfc59ab4a5fa29358be38cb8715463a8
Lucas Barbosa <soares_lucas13@hotmail.com>
2023-02-05 14:57:01 -0300

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')
 
-gulp.task('sass', function(
+gulp.task('sass', function(){
     gulp.src('./src/scss/main.scss')
         .pipe(sass())
         .pipe(gulp.dest('./dist/css'))
-))
+})
